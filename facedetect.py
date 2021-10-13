8dac99dc0cc74ab6b67d76b84de4f80bc767be63
asus <adarshnarayan0708@gmail.com>
2021-10-13 18:46:24 +0530

    Face Detection Implementation Code

---
+++ facedetect.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+src_path = r"C:\\Users\\asus\\Desktop\\CNNProject\\Resources\\Weeks56\\haarcascade_frontalface_alt2.xml"
+cascade_classifier = cv2.CascadeClassifier(src_path)
+photo = r"C:\\Users\\asus\\Desktop\\CNNProject\\Resources\\Weeks56\\photo.jpg"
+image = cv2.imread(photo)
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_image, cmap="gray")
+
+detected_face = cascade_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=10, minSize=[30,30])
+
+print(detected_face)
+
+for (x, y, width, height) in detected_face:
+    cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 10)
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+
+
+
+
---
+++ realtime.py
@@ -0,0 +1,29 @@
+import  cv2
+
+src_path = r"C:\\Users\\asus\\Desktop\\CNNProject\\Resources\\Weeks56\\haarcascade_frontalface_alt2.xml"
+cascade_classifier = cv2.CascadeClassifier(src_path)
+
+#0 means we are going to use the default camera
+video_capture = cv2.VideoCapture(0)
+
+#index-> width of capturing window,  width
+video_capture.set(3, 640)
+video_capture.set(4, 480)
+
+while True:
+    ret, img = video_capture.read()
+
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detected_faces = cascade_classifier.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=10, minSize=[15,15])
+
+    for (x, y, width, height) in detected_faces:
+        cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 5)
+
+    cv2.imshow('Real-time face detection', img)
+
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
